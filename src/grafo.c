8eac51424e4f2676f9339795d30c11396b81951f
Heitor Ventura <heitorventura21@gmail.com>
2019-11-24 23:31:35 -0300

    ajustes de indent

--- src/grafo.c
+++ src/grafo.c
@@ -12,26 +12,37 @@ Grafo* create_graph (int size)
 }
 
 Grafo* free_graph (Grafo* grafo) {
-    for (int i = 0; i < grafo->v; ++i) {
-        if (grafo->adj[i] != NULL) {
+    for (int i = 0; i < grafo->v; ++i)
+        if (grafo->adj[i] != NULL)
             free(grafo->adj[i]);
-        }
-    }
     free(grafo->adj);
     free(grafo);
     return NULL;
 }
 
-No* add_node (No* prev, int out) {
+No* add_node (No* prev, int out)
+{
     No* new = (No*) malloc(sizeof(No));
     new->id = out;
     new->next = prev;
     return new;
 }
 
-void add_edge (Grafo* grafo, int in, int out, int directional) {
-  grafo->adj[in] = add_node(grafo->adj[in], out);
-  if (!directional) {
-    grafo->adj[out] = add_node(grafo->adj[out], in);
+void add_edge (Grafo* grafo, int in, int out, int directional)
+{
+    grafo->adj[in] = add_node(grafo->adj[in], out);
+    if (!directional)
+        grafo->adj[out] = add_node(grafo->adj[out], in);
+}
+
+void print_graph (Grafo* grafo) {
+  for (int i = 0; i < grafo->v; putchar('\n'), ++i) {
+    printf("%d -> ", i);
+    No* no = grafo->adj[i];
+    while (no != NULL) {
+      printf("%d ", no->id);
+      no = no->next;
+    }
   }
+  printf("\n\n");
 }
--- src/grafo.h
+++ src/grafo.h
@@ -7,7 +7,7 @@ typedef struct _node
     /* Chave do nó */
     int id;
     /* Próximo elemento da lista encadeada */
-    No* next;
+    struct _node* next;
 } No;
 
 /* Estrutura de grafo em lista de adjacências */
@@ -29,5 +29,7 @@ Grafo* free_graph (Grafo*);
 No* add_node (No*, int);
 /* Adiciona uma aresta nova à lista de adjacências. O último atributo inteiro implica se a aresta é direcionada ou não */
 void add_edge (Grafo*, int, int, int);
+/* Imprime o grafo. */
+void print_graph (Grafo* grafo);
 
 #endif
